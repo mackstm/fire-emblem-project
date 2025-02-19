aa0692228d64b0a8f86cd392265b90322097ebf1
mackstm <thelewyntm@gmail.com>
2025-02-19 16:15:21 +0000

    feat: I can't take this anymore

--- code/fe-project/src/views/FireEmblemGame.vue
+++ code/fe-project/src/views/FireEmblemGame.vue
@@ -4,51 +4,46 @@
     <h3 class="animate-pulse">Cargando personajes</h3>
   </section>
   <section v-else>
-    <h1>Adivinar personaje de Fire Emblem: The Sacred Stones</h1>
+    <img src="../../images/assets/fe8-logo.png" alt="logo" >
+    <h3 class="mt-2">
+          {{ gameStatus === GameStatus.Playing ? 'Jugando...' : 'Intentalo de nuevo en: ' + restartCounter }}
+    </h3>
 
-    <div class="md:flex w-[65%] justify-between">
-      <div class="md:flex flex-col items-start">
-        <h3 class="mt-2">
-          {{ gameStatus === GameStatus.Playing ? 'Adivina el personaje' : 'Intentalo de nuevo en: ' + restartCounter }}
-        </h3>
+    <div class="flex flex-col md:flex-row w-[65%] justify-evenly items-center">
+      <div class="flex flex-col items-center">
 
-        <h3 class="mt-2" v-if="gameStatus !== GameStatus.Playing">
+
+          <h3 class="mb-3" v-if="gameStatus !== GameStatus.Playing">
           {{
             randomUnit.Name.toLocaleUpperCase()}}
-        </h3>
-        <FireEmblemPicture :unitName="randomUnit.Name.toLowerCase()"
-        :showUnit="gameStatus !== GameStatus.Playing"/>
+          </h3>
+          <FireEmblemPicture :unitName="randomUnit.Name.toLowerCase()"
+          :showUnit="gameStatus !== GameStatus.Playing"/>
 
-        <form @submit.prevent="sendAnswer">
-          <input v-if="gameStatus != GameStatus.Playing" v-model="guess" type="text"  placeholder="Adivina!" disabled />
-          <input v-else type="text" v-model="guess" placeholder="Escribe aquí" />
-          <v-btn type="submit" :disabled="gameStatus !== GameStatus.Playing"
-            base-color="gray"
-            active-color="success"
-            text="Adivina!"
-            :active="gameStatus == GameStatus.Playing"
-          ></v-btn>
-        </form>
 
-        <div class="text-sm mt-2">
-          Victorias: {{ winCount }}
-        </div>
+
 
       </div>
-      <div class="md:flexflex-col">
 
 
-        <div class="text">
+      <v-card class="bg-black mb-4" elevation="16" max-width="344" min-height="38rem">
+        <v-card-item>
+          <v-card-title> Pistas </v-card-title>
+          <v-card-subtitle> Acerca del personaje... </v-card-subtitle>
+        </v-card-item>
+        <v-card-text class="h-1/12">
           <ul>
             <li v-for="(item, index) in cluesARR" :key="index">
                 <ul v-if="index == 1">
                   <li class="flex justify-between items-center mt-1" v-for="(item2, index2) in item.split(';')" :key="index2">
-                    <p class="me-2">
+                    <p class="font-bold me-2">
                       {{ item2.split(':')[0] }}
                     </p>
 
                     <v-progress-circular
+                      class="font-bold"
                       color="green"
+                      bg-color="#7d7d7d"
                       :model-value="calcStatForBar(item2.split(':'))"
                       size="50"
                     >
@@ -57,11 +52,53 @@
                   </li>
                 </ul>
 
-                <p v-else>{{ item }}</p>
+                <p class="font-bold" v-else>{{ item }}</p>
             </li>
           </ul>
-        </div>
-      </div>
+        </v-card-text>
+      </v-card>
+
+
+
+    </div>
+
+
+      <form class="flex flex-row justify-center items-center" @submit.prevent="sendAnswer">
+          <!-- <input class="bg-white h-[10px]" v-if="gameStatus != GameStatus.Playing"type="text"  placeholder="Escribe aquí" disabled />
+          <input class="bg-white h-10 rounded-full" v-else type="text" v-model="guess" placeholder="Escribe aquí" /> -->
+          <v-text-field
+            v-if="gameStatus != GameStatus.Playing"
+            name="Adivina"
+            label="Escribe aquí"
+            id="id"
+            width="200"
+            disabled
+          ></v-text-field>
+
+          <v-text-field
+            v-else
+            name="Adivina"
+            label="Escribe aquí"
+            id="id"
+            width="200"
+
+            background-color="success"
+            v-model="guess"
+          ></v-text-field>
+
+          <v-btn type="submit" :disabled="gameStatus !== GameStatus.Playing"
+            class="ms-10"
+            base-color="gray"
+            active-color="#eac476"
+            text="Adivina!"
+            :active="gameStatus == GameStatus.Playing"
+          ></v-btn>
+        </form>
+
+
+
+    <div class="text-sm mt-2">
+      Victorias: {{ winCount }}
     </div>
 
   </section>
